ad95e845c68996bfe652406c72331c510ef8b5ff
sumitsingh-kiwi <sumit.singh@kiwitech.com>
2021-10-20 19:24:37 +0530

    test script run

--- google-function-example/main.py
+++ google-function-example/main.py
@@ -13,4 +13,4 @@ def leasera(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello World!'
+        return f'Hello World again!'
