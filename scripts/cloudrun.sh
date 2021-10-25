a8f76892d58e85c55d55981c485cf926f028ed4c
sumitsingh-kiwi <sumit.singh@kiwitech.com>
2021-10-25 17:36:02 +0530

    added cloudrun script

---
+++ scripts/cloudrun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TIME=`date +%b-%d-%y`
+FILENAME=Leasera-service-backup-$TIME
+tar -czvf $FILENAME.tar.gz .
+gsutil cp $FILENAME.tar.gz gs://build-code-backup/
+
+gcloud run deploy helloworld --source=hello_world --region=us-central1 --allow-unauthenticated
