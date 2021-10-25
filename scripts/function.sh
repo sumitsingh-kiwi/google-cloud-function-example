80fe29a2cb08713c55d2f7d076f1f4fda0c6c75d
sumitsingh-kiwi <sumit.singh@kiwitech.com>
2021-10-25 13:37:59 +0530

    created a bash script to call from cloudbuild

--- scripts/function.sh
+++ scripts/function.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-functions deploy leasera --region=us-central1 --source=google-function-example --trigger-http --runtime=python37
+TIME=`date +%b-%d-%y`
+FILENAME=backup-$TIME
+tar -czvf $FILENAME.tar.gz .
+gsutil cp $FILENAME.tar.gz gs://build-code-backup/
+
+gcloud functions deploy leasera --region=us-central1 --source=google-function-example --trigger-http --runtime=python37
+
+#gcloud functions deploy publish --region=us-central1 --source=google-pub-sub-example --trigger-http --runtime=python37
+
+#gcloud functions deploy subscribe --region=us-central1 --source=google-pub-sub-example --trigger-topic queueDemo --runtime=python37
